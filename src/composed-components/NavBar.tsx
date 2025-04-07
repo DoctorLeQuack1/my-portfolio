6a777236234ce411271b52c026b6b0d1623fdc2b
Sergio Arturo Aguilar Basurto <sergioaguilar@Laptop-de-Sergio.local>
2025-04-07 13:26:49 -0600

    Fixed resize issue

--- src/composed-components/NavBar.tsx
+++ src/composed-components/NavBar.tsx
@@ -4,7 +4,7 @@ export const NavBar = () => {
 
     return(
 
-        <div className="flex flex-row flex-wrap justify-between items-center py-5 px-10">
+        <div className="flex flex-row flex-nowrap justify-between items-center py-5 px-10 overflow-x-auto whitespace-nowrap gap-10">
 
             <img src="/astronaut.png" className="w-full h-full max-w-28 max-h-28" />
             <div className="flex flex-row justify-center items-center gap-10">
