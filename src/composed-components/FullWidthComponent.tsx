22cac8969548dc9d698686448b3d1a1fcd38f36e
Sergio Arturo Aguilar Basurto <sergioaguilar@Laptop-de-Sergio.local>
2025-04-08 20:15:13 -0600

    Fixed screen sizing

---
+++ src/composed-components/FullWidthComponent.tsx
@@ -0,0 +1,7 @@
+export const FullWidthComponent = () => {
+    return (
+        <div className="w-full h-auto bg-black text-white">
+            <h1 className="text-4xl font-bold">¡Este componente ocupa todo el ancho!</h1>
+        </div>
+    );
+};
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,16 +1,18 @@
 export const AboutMe = () => {
     return (
-        <div className="grid grid-col-1 lg:grid-cols-2 w-full">
-            <div className="flex flex-col justify-center items-center lg:items-baseline mx-20">
-                <h1 className="text-7xl text-gray-600 pb-5 text-center lg:text-start">Hi, I am Sergio Aguilar,</h1>
-                <p className="text-2xl text-gray-600 pb-5 text-center lg:text-start">I am a Sr. Software Engineer with 4 years of experience, passionate about learning and always eager to face new challenges.</p>
-                <div className="flex flex-row gap-1 justify-start">
-                    <p className="text-xl text-gray-600">Check some of the work I have done:</p>
-                    <a href="https://github.com/DoctorLeQuack1" target="_blank" rel="noopener noreferrer"><img className="max-h-[30px] max-w-[30px]" src="/github-mark.svg" /></a>
+        <div className="flex flex-row justify-center items-center w-full min-h-screen">
+            <div className="grid grid-col-1 lg:grid-cols-2 w-full">
+                <div className="flex flex-col justify-center items-center lg:items-baseline mx-20">
+                    <h1 className="text-7xl text-gray-600 pb-5 text-center lg:text-start">Hi, I am Sergio Aguilar,</h1>
+                    <p className="text-2xl text-gray-600 pb-5 text-center lg:text-start">I am a Sr. Software Engineer with 4 years of experience, passionate about learning and always eager to face new challenges.</p>
+                    <div className="flex flex-row gap-1 justify-start">
+                        <p className="text-xl text-gray-600">Check some of the work I have done:</p>
+                        <a href="https://github.com/DoctorLeQuack1" target="_blank" rel="noopener noreferrer"><img className="max-h-[30px] max-w-[30px]" src="/github-mark.svg" /></a>
+                    </div>
                 </div>
-            </div>
-            <div className="flex justify-center lg:justify-end lg:mr-20">
-                <img className="w-full max-w-md h-auto object-contain" src="/avatar.png" alt="" />
+                <div className="flex justify-center lg:justify-end lg:mr-20">
+                    <img className="w-full max-w-md h-auto object-contain" src="/avatar.png" alt="" />
+                </div>
             </div>
         </div>
     );
